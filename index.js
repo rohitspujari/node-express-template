9154a575e0dfd51384d31fa4368bfe9b1d6ff7ba
Rohit Pujari <rohitspujari@gmail.com>
2017-07-29 12:27:06 -0500

    added nodemon

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 
 const app = express();
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 app.get('/', (req, res) => {
-  res.send({ message: 'hello world' });
+  res.send({ message: 'hello world!' });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
+//secret hQIGd3WM3qYDuBNdjZmOGzlY
