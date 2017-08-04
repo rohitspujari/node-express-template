c834e3f7760b36c7f788243a982106d357f195e3
Rohit Pujari <rohitspujari@gmail.com>
2017-08-04 00:17:41 -0500

    added server side logic to serve static react assets, added heroku build step

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.use(passport.session());
 require('./routes/authRoute')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets
+  //like main.js and main.css file
+  app.use(express.static('client/build'));
+
+  //Express will serve up index.html file
+  //if it doesn't see the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
